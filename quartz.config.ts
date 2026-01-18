e9de01f02e82651a74e7890e94e7920072a9b8f6
sagiri <i@sagiri.me>
2026-01-18 10:15:18 +0800

    改网站名称和图标

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,14 +8,14 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "叹息六月",
+    pageTitle: "六月笔记",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
     analytics: null,
     locale: "zh-CN",
     baseUrl: "sighjune.com",
-    ignorePatterns: ["private", "templates", ".obsidian"],
+    ignorePatterns: ["private", "templates", ".obsidian", "模板"],
     defaultDateType: "modified",
     theme: {
       fontOrigin: "googleFonts",
